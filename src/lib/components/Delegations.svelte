768394da36ad3a259599460936248f97377a267c
kent-3 <100624004+kent-3@users.noreply.github.com>
2023-06-09 23:42:24 -0400

    formatting

--- src/lib/components/Delegations.svelte
+++ src/lib/components/Delegations.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-	import { chains } from '$lib/config';
-	import { secretClient, secretAddress } from '$lib/stores';
-	import { SecretNetworkClient } from 'secretjs';
+	import { secretClient, secretAddress, isAccountAvailable } from '$lib/stores';
 	import type {
-		Delegation,
 		DelegationResponse,
 	} from 'secretjs/dist/grpc_gateway/cosmos/staking/v1beta1/staking.pb';
-	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 
 	async function getDelegations(): Promise<DelegationResponse[]> {
@@ -20,29 +16,31 @@
 
 <div class="card w-full space-y-2 bg-surface-50 p-4 text-left dark:!bg-[#28292a]">
 	<h2 class="!text-3xl font-bold tracking-wide">Delegations</h2>
-	{#await getDelegations()}
-		<p class="font-mono font-bold text-secondary-800 dark:text-primary-600">loading</p>
-	{:then responses}
-		{#if responses.length > 0}
-			{#each responses as response}
-				<p class="font-mono font-bold text-secondary-800 dark:text-primary-600">
-					{response.delegation?.validator_address}
+	{#key $isAccountAvailable}
+		{#await getDelegations()}
+			<p class="font-mono font-bold text-secondary-800 dark:text-primary-600">loading</p>
+		{:then responses}
+			{#if responses.length > 0}
+				{#each responses as response}
+					<p class="font-mono font-bold text-secondary-800 dark:text-primary-600">
+						{response.delegation?.validator_address}
+					</p>
+				{/each}
+			{:else}
+				<p
+					in:fade={{ duration: 600 }}
+					class="font-mono font-bold text-secondary-800 dark:text-primary-600"
+				>
+					None
 				</p>
-			{/each}
-		{:else}
+			{/if}
+		{:catch}
 			<p
 				in:fade={{ duration: 600 }}
 				class="font-mono font-bold text-secondary-800 dark:text-primary-600"
 			>
-				None
+				Not Connected
 			</p>
-		{/if}
-	{:catch}
-		<p
-			in:fade={{ duration: 600 }}
-			class="font-mono font-bold text-secondary-800 dark:text-primary-600"
-		>
-			Not Connected
-		</p>
-	{/await}
+		{/await}
+	{/key}
 </div>
